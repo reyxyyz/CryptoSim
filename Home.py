4dd79649edb0f7faa95d97b9a9321b445a1f1489
reyxyyz <sitimarifatulmusaadahh@gmail.com>
2025-06-06 07:43:59 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,18 @@
+# File: Home.py
+import streamlit as st
+
+st.set_page_config(page_title="CryptoApp", layout="centered")
+
+st.title("🔐 CryptoApp: Simulasi Kriptografi Interaktif")
+st.markdown("""
+Selamat datang di **CryptoApp**, aplikasi interaktif untuk mempelajari dan mencoba algoritma kriptografi klasik dan modern.
+
+Navigasi di sidebar untuk:
+- 🔁 Enkripsi & Dekripsi teks
+- 📊 Membandingkan algoritma
+- 🧠 Belajar kriptografi secara interaktif
+
+---
+
+**Dibuat untuk Tugas UAS - Pemrograman Kriptografi**
+""")
